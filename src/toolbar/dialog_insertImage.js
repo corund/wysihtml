de6ff601f9c6d14fdc2ab0ccc70a66b7a27d0378
Jin <jin@iropke.com>
2016-09-05 14:40:21 +0900

    view image preview in insertImageDialog

---
+++ src/toolbar/dialog_insertImage.js
@@ -0,0 +1,12 @@
+(function(wysihtml5) {
+  wysihtml5.toolbar.Dialog_insertImage = wysihtml5.toolbar.Dialog.extend({
+    _interpolate: function(avoidHiddenFields) {
+      this.base(avoidHiddenFields);
+      var imgView = this.container.querySelector('.qq-container IMG');
+      if (null !== imgview) {
+        var newValue  = (this.elementToChange && typeof(this.elementToChange) !== 'boolean') ? (this.elementToChange.getAttribute("SRC") || "") : field.defaultValue;
+        imgView.setAttribute("SRC", newValue);
+      }
+    }
+  });
+})(wysihtml5);
